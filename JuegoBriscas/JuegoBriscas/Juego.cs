e6d55f6dc59eee4556a4c013b677808fa9e18dbb
Hector <hcortizcolon@gmail.com>
2017-04-29 18:17:44 -0400

    Comenzar el proyecto

---
+++ JuegoBriscas/JuegoBriscas/Juego.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace JuegoBriscas
+{
+	public class Juego
+	{
+		Jugador jugador1;
+		Jugador jugador2;
+
+
+		public Juego ()
+		{
+		}
+
+		public void pantallaIincio()
+		{
+		}
+
+		public void inicializar()
+		{
+		}
+
+		public void comenzarJuego()
+		{
+		}
+		//Lo que falte
+	}
+}
+
---
+++ JuegoBriscas/JuegoBriscas/Jugador.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace JuegoBriscas
+{
+	public class Jugador
+	{
+		int[] cartas;
+		int[] mano;
+		Palo vida;
+		int puntos;
+		int cartaARobar;
+
+		public Jugador ()
+		{
+		}
+
+		public void barajar()
+		{
+		}
+
+		public void repartir()
+		{
+		}
+
+		public void recibirJugada(int carta)
+		{
+		}
+
+		public int procesarJugada()
+		{
+		}
+
+		public void robarCartas()
+		{
+		}
+
+		public void mostrarJuego()
+		{
+		}
+		//Lo que falte
+	}
+}
+
